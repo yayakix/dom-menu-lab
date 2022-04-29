30de17ba0a26fea14b7b02ff8dacd90db746fcfc
Iyana Marquez <yayakix@gmail.com>
2022-04-29 19:22:58 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Menu data structure
+var menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
+let mainEl = document.querySelector("main");
+mainEl.style.backgroundColor;
