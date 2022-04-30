d6843e0f1f1441cdf838053f30c798c1c1cd39ac
Iyana Marquez <yayakix@gmail.com>
2022-04-30 00:34:12 -0400

    finished part 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+let mainEl = document.querySelector("main");
+// let mainElColor = mainEl.style.getPropertyValue("--main-bg");
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = "SEI Rocks!!";
+mainEl.classList.add("flex-ctr");
+
+let topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+// let menuHeight = topMenuEl.style.getPropertyValue("--top-menu-bg");
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.style.height = "100%";
+topMenuEl.classList.add("flex-around");
+
 // Menu data structure
 var menuLinks = [
   { text: "about", href: "/about" },
@@ -6,5 +19,9 @@ var menuLinks = [
   { text: "account", href: "/account" },
 ];
 
-let mainEl = document.querySelector("main");
-mainEl.style.backgroundColor;
+for (let i = 0; i < menuLinks.length; i++) {
+  const aElement = document.createElement("a");
+  aElement.href = menuLinks[i].href;
+  aElement.textContent = menuLinks[i].text;
+  topMenuEl.appendChild(aElement);
+}
